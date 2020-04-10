731c37d14b029cf3fb0f55f9620c33c084052dd2
Fadlan Kasyfi <55770934+FadlanKasyfi2@users.noreply.github.com>
2020-04-10 11:00:38 +0700

    Create aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+echo "Nama : Fadlan Kasyfi<br>";
+echo "Kelas : XI RPL 1<br>";
+echo "Asal Sekolah : SMK Taruna Bhakti<br>";
+echo "Alamat : Perum Jatijajar Blok D 24/16<br>";
+
+?>
+
+</body>
+</html>
